790a3a3fa412a17484313dc85239244c8c2ffc93
George AbdElmesseh <76843136+George-Abdelmessh@users.noreply.github.com>
2022-12-16 18:33:28 +0200

    done

--- main.py
+++ main.py
@@ -1,22 +1,6 @@
-
-import cv2 as cv
-from easyocr import Reader
-
 '''' 
     TODO Before Run Read [README.md] file
 '''
-
-'''' 
-    TODO Before Run Read [README.md] file
-    TO Run The Project You Must Have 
-    Python3, pip3, easyocr, opencv and opencv-contrib
-    COMMANDS:
-        pip install easyocr 
-        pip install opencv-python 
-        pip install opencv-contrib-python 
-'''
->>>>>>> 0ebc77ee40fb42db71ffb9c9050be6db336e4a85
-
 
 from view import ui as UI
 
